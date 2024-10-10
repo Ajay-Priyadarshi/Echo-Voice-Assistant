eafdd10aa49d9105806541b47784571376b8abd5
Ajay Priyadarshi <ajbj444@gmail.com>
2024-10-10 22:49:59 +0530

    espeak

--- commands/utils.py
+++ commands/utils.py
@@ -1,7 +1,6 @@
 import speech_recognition as sr
 import os
 
-# State variable to track if the assistant is awake
 is_awake = True
 
 def say(text):
@@ -13,15 +12,13 @@ def takeCommand(listen_for_wake=False):
     with sr.Microphone() as source:
         if not listen_for_wake:
             recognizer.adjust_for_ambient_noise(source, duration=1)
-            print("Listening...")  # Active listening message
+            print("Listening...")  
         else:
-            # Optional: Adjust parameters for wake word detection if needed
             recognizer.pause_threshold = 1
-            print("Listening for wake command...")  # Distinct message for sleep mode
+            print("Listening for wake command...")  
 
         try:
             if listen_for_wake:
-                # Timeout for wake word detection
                 audio = recognizer.listen(source, timeout=5000, phrase_time_limit=5000)  # 10 seconds for wake word detection
             else:
                 audio = recognizer.listen(source, timeout=3, phrase_time_limit=3)  # Shorter timeout for active listening
